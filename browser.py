f4c60532d86d6a4d762ebc5e1c275db80152da55
ArturYumagulov <zico.13288@gmail.com>
2024-08-16 16:57:26 +0300

    1. Добавил парсинг url страниц с точками

--- browser.py
+++ browser.py
@@ -1,7 +1,8 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.common import NoSuchElementException
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 
 
@@ -9,6 +10,7 @@ class Browser:
     def __init__(self):
         self.driver: webdriver = webdriver.Chrome()
         self.urls_list = []
+        self.sleep_time = 2
 
     def search_2gis_form(self, text: str) -> None:
         """Передача данных в поле поиска и нажатие кнопки ENTER"""
@@ -16,7 +18,7 @@ class Browser:
         search_form = self.driver.find_element(By.XPATH, '//input[@enterkeyhint="search"]')
 
         search_form.send_keys(text)
-        time.sleep(1)
+        time.sleep(self.sleep_time)
         search_form.send_keys(Keys.ENTER)
 
     def get_point_data(self, url) -> None:
@@ -27,7 +29,7 @@ class Browser:
         print(self.driver.find_element(By.CLASS_NAME, '_1p8iqzw').text)
         phone_block = self.driver.find_element(By.CLASS_NAME, '_b0ke8')
         phone_block.find_element(By.TAG_NAME, 'button').click()
-        time.sleep(2)
+        time.sleep(self.sleep_time)
         phone = self.driver.find_element(By.TAG_NAME, 'bdo').text
         print(phone)
         email = self.driver.find_element(By.CSS_SELECTOR, 'div._599hh > div:nth-child(4)').text
@@ -54,26 +56,52 @@ class Browser:
         filters.click()
         time.sleep(1)
 
-        # click paginator
+        # pages_count
+        point_count = int(self.driver.find_element(By.CLASS_NAME, "_1xhlznaa").text) // 12
 
-        paginator = self.driver.find_element(
-            By.XPATH,
-            '//*[@id="root"]/div/div/div[1]/div[1]/div[3]/div/div/div[2]/div/div/div/div[2]/div[2]/div[1]/div/'
-            'div/div/div[3]')
-        pages = paginator.find_elements(By.TAG_NAME, 'a')
-        for page in pages:
-            page.click()
-            time.sleep(2)
+        paginator_next = self.driver.find_element(
+            By.CSS_SELECTOR, '._5ocwns > div:nth-child(2) > svg')
 
-        # urls = self.driver.find_elements(By.CSS_SELECTOR, 'div._zjunba > a')
-        # print(len(urls))
-        #
-        # # parse urls
-        # for url in urls:
-        #     url = url.get_attribute('href')
-        #     self.urls_list.append(url)
-        #
-        # print(len(self.urls_list))
+        for points_page in range(1, point_count + 1):
+            time.sleep(self.sleep_time)
+            urls = self.driver.find_elements(By.CSS_SELECTOR, 'div._zjunba > a')
+
+            if len(urls) > 0:
+                for url in urls:
+                    if url.get_attribute('href'):
+
+                        url = url.get_attribute('href')
+                        sub_browser = webdriver.Chrome()
+                        sub_browser.get(url)
+
+                        time.sleep(3)
+
+                        # self.driver.find_element(By.CLASS_NAME, "_euwdl0").click()
+
+                        print(sub_browser.find_element(By.CLASS_NAME, '_tvxwjf').text)
+                        # print(sub_browser.find_element(By.CLASS_NAME, '_oqoid').text)
+                        try:
+                            address = sub_browser.find_element(By.CLASS_NAME, '_oqoid')
+                        except NoSuchElementException:
+                            address = sub_browser.find_element(By.CLASS_NAME, "_er2xx9")
+                        print(address.text)
+                        print(sub_browser.find_element(By.CLASS_NAME, '_1p8iqzw').text)
+                        # phone_block = sub_browser.find_element(By.CLASS_NAME, '_b0ke8')
+                        # phone_block.find_element(By.TAG_NAME, 'button').click()
+                        # time.sleep(self.sleep_time)
+                        # phone = sub_browser.find_element(By.TAG_NAME, 'bdo').text
+                        # print(phone)
+                        # email = sub_browser.find_element(By.CSS_SELECTOR, 'div._599hh > div:nth-child(4)').text
+                        # print(email)
+                        time.sleep(self.sleep_time)
+                        sub_browser.quit()
+
+            time.sleep(self.sleep_time)
+
+            # scroll_to_points_elements
+            actions = ActionChains(self.driver)
+            actions.move_to_element(paginator_next).perform()
+            paginator_next.click()
 
 
 if __name__ == '__main__':
